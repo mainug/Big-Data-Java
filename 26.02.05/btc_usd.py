5ac38afcfa2ca8fc07d0d74d2d51a99382ea5d38
박민욱 <48702167+mainug@users.noreply.github.com>
2026-02-05 16:19:22 +0900

    Add files via upload

---
+++ 26.02.05/btc_usd.py
@@ -0,0 +1,8 @@
+import requests as r
+btc = "https://api4.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+usd = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=3spOQb36RyxH4cbtYdC1igtjlbe6K1YU&data=AP01"
+r1 = r.get(btc).json()
+r2 = r.get(usd).json()
+num = r2[-1]['deal_bas_r']
+r3 = float(r1['price']) * float(num.replace(',', ''))
+print(r3, type(r3))
